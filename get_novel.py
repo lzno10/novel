2d5d695ce094ca2b74ae2562bf55054af9432b1f
genglinlin <genglinlinneu@163.com>
2018-01-29 23:02:53 +0800

    first commit

---
+++ get_novel.py
@@ -0,0 +1,14 @@
+#import urllib
+#import urllib2
+#from lxml import etree
+# url = "http://www.jjwxc.net/onebook.php?novelid=175726"
+url = "http://blog.csdn.net/jerry_1126/article/details/43758179"
+#req_header = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'}
+#req_timeout = 120
+#req = urllib2.Request(url,None,req_header)
+#ret = urllib2.urlopen(req,None,req_timeout).read()
+# print ret
+#html = etree.HTML(ret)
+#title = html.xpath('/html/head/title')
+#print title[0].text
+print url
